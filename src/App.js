48e684b2605d8c34ad2c4301954981c71aa31dd0
JD <114588726+mhVxVNMwXkm9s4Ze9VASei@users.noreply.github.com>
2023-07-15 23:35:47 -0500

    Added state!
    yeet.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const messages = [
 	"Learn React ⚛️",
 	"Apply for jobs 💼",
@@ -5,14 +7,14 @@ const messages = [
 ];
 
 export default function App() {
-	const step = 2;
+	const [step, setStep] = useState(1);
 
 	function handleNext() {
-		alert("Next.");
+		setStep(step < messages.length ? step + 1 : step);
 	}
 
 	function handlePrevious() {
-		alert("Previous.");
+		setStep(step > 1 ? step - 1 : step);
 	}
 
 	return (
